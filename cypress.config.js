db5106eb957ed5a707209ac21d0b09dc428899ae
Olufemi Awe <mar4r8kfemi@gmail.com>
2025-03-02 21:27:23 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,11 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
+  projectId: "sjnkd5",
+  video: true,
+  videoUploadOnPasses: false,
   e2e: {
-    baseUrl: "http://localhost:3000",
+    // e2e testing configuration
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
-
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
-});
+    }
+  }
+}
